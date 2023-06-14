e40b9f41a7158b373ecd161fd6d893247b97c837
Bogdan Calinescu :D <93613908+bogdan2002VS@users.noreply.github.com>
2023-06-14 12:43:36 +0200

    changes <3

--- src/test/java/com/veganny/business/jwt/AccessTokenHelperTest.java
+++ src/test/java/com/veganny/business/jwt/AccessTokenHelperTest.java
@@ -1,60 +1,68 @@
 package com.veganny.business.jwt;
 
+import com.veganny.domain.AccessToken;
 import com.veganny.domain.User;
-import com.veganny.domain.impl.AdminRole;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import com.veganny.exception.BadTokenException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-class AccessTokenHelperTest {
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccessTokenHelperTest {
 
     @Mock
-    private IAccessTokenHelper accessTokenHelper;
+    private User user;
 
-    @Test
-    @DisplayName("Should return null when the user is null")
-    void generateAccessTokenWhenUserIsNull() {
-        User user = null;
-        String accessToken = accessTokenHelper.generateAccessToken(user);
-        assertNull(accessToken);
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @InjectMocks
+    private AccessTokenHelper accessTokenHelper;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
-
-
     @Test
-    @DisplayName("Should generate a valid access token for a given user")
-    void generateAccessTokenForGivenUser() {
-        AdminRole adminRole = new AdminRole("Admin", 1L);
-        User user =
-                User.builder()
-                        .id(1L)
-                        .role(adminRole)
-                        .username("john.doe")
-                        .password("password")
-                        .firstName("John")
-                        .lastName("Doe")
-                        .email("john.doe@example.com")
-                        .address("123 Main St")
-                        .phone("555-555-5555")
-                        .build();
-
-        when(accessTokenHelper.generateAccessToken(user))
-                .thenReturn(
-                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+    public void testGenerateAccessToken() {
+        when(user.getId()).thenReturn(1L);
+        when(user.getUsername()).thenReturn("testuser");
+        when(user.getRole()).thenReturn(null);
 
         String accessToken = accessTokenHelper.generateAccessToken(user);
 
         assertNotNull(accessToken);
-        assertEquals(
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-                accessToken);
-        verify(accessTokenHelper, times(1)).generateAccessToken(user);
+        assertTrue(accessToken.length() > 0);
+    }
+
+    @Test
+    public void testDecode() {
+        when(user.getId()).thenReturn(1L);
+        when(user.getUsername()).thenReturn("testuser");
+        when(user.getRole()).thenReturn(null);
+
+        String accessToken = accessTokenHelper.generateAccessToken(user);
+
+        AccessToken decodedToken = accessTokenHelper.decode(accessToken);
+
+        assertNotNull(decodedToken);
+        assertEquals(decodedToken.getUsername(), "testuser");
+        assertNull(decodedToken.getRole());
+        assertEquals(decodedToken.getUserId(), 1L);
+    }
+
+    @Test
+    public void testDecodeWithInvalidToken() {
+        assertThrows(BadTokenException.class, () -> accessTokenHelper.decode("invalid_token"));
     }
 }
